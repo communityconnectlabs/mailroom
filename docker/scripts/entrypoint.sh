b83236b20dafcc6cb58d42b63c863c8c93e35a39
noizu <keith.brings@noizu.com>
2024-12-03 01:10:27 +0700

    entrypoint.sh script update for sandbox. Remove docs symlink as we mount it as a volume instead to avoid sideffects

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /app || exit 1
-ln -s ./docs-exampls docs
-go install -v ./cmd/mailroom && chmod +x ./mailroom || exit 1
-./mailroom
+go install -v ./cmd/mailroom && chmod +x /go/bin/mailroom || exit 1
+/go/bin/mailroom
