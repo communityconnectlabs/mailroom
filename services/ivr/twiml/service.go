aa6ac7a3f4ff2ab8a3e12a7df35560b344ac8b29
Ihor Palii <palii.ihor@uvik.net>
2025-07-11 15:28:08 +0300

    Added japanese

--- services/ivr/twiml/service.go
+++ services/ivr/twiml/service.go
@@ -165,6 +165,7 @@ var languageCountryCodes = map[string]envs.Country{
 	"kor": envs.Country("KR"),
 	"rus": envs.Country("RU"),
 	"tha": envs.Country("TH"),
+	"jpn": envs.Country("JP"),
 }
 
 var indentMarshal = true
